a742e21fc1b74976bf06a9cff5737a5c9450694a
YL <simon-ling@hotmail.com>
2021-06-18 22:37:00 -0400

    t0822 is done

--- src/main/java/com/dailycodebuffer/kafka/DemoController.java
+++ src/main/java/com/dailycodebuffer/kafka/DemoController.java
@@ -2,19 +2,19 @@ package com.dailycodebuffer.kafka;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
     @Autowired
 //  kafka msg is string by default
-    KafkaTemplate<String,String> kafkaTemplate;
+    KafkaTemplate<String,Book> kafkaTemplate;
     private static final String TOPIC="NewTopic";
-    @GetMapping("/publish/{msg}")
-    public String publishMsg(@PathVariable("msg") final String msg){
-        kafkaTemplate.send(TOPIC,msg);
+    @PostMapping("/publish")
+    public String publishMsg(@RequestBody Book book){
+        kafkaTemplate.send(TOPIC,book);
         return "Published Successfully";
     }
 }
